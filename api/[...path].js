c918b32a227f0b186ecc92a67436d90078b4143d
davidokpo <davidokpo7@gmail.com>
2026-04-21 13:52:58 +0100

    Reduce Vercel API to a single function

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,3 @@
-import app from './src/main.js';
+import app from '../server/src/main.js';
 
 export default app;
--- server/src/env.js
+++ server/src/env.js
@@ -5,5 +5,4 @@ import dotenv from 'dotenv';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
+dotenv.config({ path: path.resolve(__dirname, '../../api/.env') });
